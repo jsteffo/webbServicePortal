f46d7684614bec7eed5aa5987928a612e7ff511b
stefan <stefan@stefan-LIFEBOOK-UH552>
2015-04-20 11:29:42 +0200

    initial commit

---
+++ SecondWebbPortal/src/resources/ControllerEJB.java
@@ -0,0 +1,24 @@
+package resources;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class ControllerEJB {
+
+	//Langa in DTO från bean i view sannolikt....
+	public void broadbandRequest(){
+		
+	}
+	
+	public void firewallRequest(){
+		
+	}
+	public void webServerRequest(){
+		
+	}
+	
+	//Kanske langa en generic request?
+	//Finns möjlighet att göra detta i ett mer dynamiskt fashion?
+	//Blir väl bättre att börja enkelt...
+	
+}
